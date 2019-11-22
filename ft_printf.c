10d9c963621e64083543ac92ec4f4e0c7c93aba3
Дмитрий Задоя <dmitry@MacBook.local>
2019-11-22 16:38:43 +0300

    adding dot in structure

--- ft_printf.c
+++ ft_printf.c
@@ -1,22 +1,21 @@
 #include "ft_printf.h"
-#include "libft/libft.h"
 
-char	ft_istype(char c)
+int		ft_istype(char c)
 {
-	char	type[] = "dioxXucsfeEgGpnf%";
+	char	type[] = "dioxXucsfeEgGpn%";
 	int		i;
 	
 	i = 0;
 	while (type[i])
 	{
 		if (type[i] == c)
-			return (type[i]);
+			return (i);
 		i++;
 	}
-	return(0);
+	return(-1);
 }
 
-char	ft_isflag(char c)
+int		ft_isflag(char c)
 {
 	char	flags[] = "#0-+ ";
 	int		i;
@@ -34,10 +33,8 @@ char	ft_isflag(char c)
 int		formated(char **string, t_specifications *fmt)
 {	
 	int		i;
-	char	dot;
 	char	*str;
 
-	dot = 0;
 	str = *string;
 	while (str)
 	{
@@ -53,7 +50,7 @@ int		formated(char **string, t_specifications *fmt)
 				fmt->sign[0] = *str;
 				fmt->sign[1] = 0;
 			}
-			else if (ft_istype(*str))
+			else if (ft_istype(*str) >= 0)
 			{
 				i = 0;
 				while(fmt->sign[i])
@@ -64,14 +61,14 @@ int		formated(char **string, t_specifications *fmt)
 		}
 		else if (*str == '.')
 		{
-			if (dot == 0)
-				dot++;
+			if (fmt->dot == 0)
+				fmt->dot++;
 			else
 				return (1);
 		}
 		else if (ft_isdigit(*str) && *str != '0')
 		{
-			if (dot == 0)
+			if (fmt->dot == 0)
 			{
 				fmt->width = ft_atoi(str);
 				while (ft_isdigit(*str))
@@ -91,31 +88,6 @@ int		formated(char **string, t_specifications *fmt)
 	}
 	*string = str;
 	return (1);
-}
-
-int            mod_c(t_specifications *data, va_list *ap)
-{
-    char     c;
-    int     result;
-
-    if (data->width > 1)
-        result = data->width;
-    else
-        result = 1;
-    c = va_arg(*ap, int);
-    if (data->flag[2] == '-')
-    {
-        ft_putchar(c);
-        while ((data->width)-- > 1)
-            ft_putchar(' ');
-    }
-    else
-    {
-        while ((data->width)-- > 1)
-            ft_putchar(' ');
-        ft_putchar(c);
-    }
-    return (result);
 }
 
 int		ft_printf(char *format, ...)
@@ -143,7 +115,7 @@ int		ft_printf(char *format, ...)
 			fmt.width = 0;
 			str++;	
 			if (formated(&str, &fmt))
-				quantity += mod_c(&fmt, &arg_ptr);
+				quantity += find_function(&fmt, &arg_ptr);
 		}
 		str++;
 	}
--- ft_printf.h
+++ ft_printf.h
@@ -2,15 +2,33 @@
 #define FT_PRINTF
 
 #include <stdarg.h>
+#include "libft/libft.h"
 
 typedef struct  s_specifications
 {
     char    flag[6];
     int     width;
     int     precision;
+    char    dot;
     char    sign[4];
 }               t_specifications;
 
+/*typedef struct  s_func_list
+{
+    t_func_list *next;
+    void        *data_type;
+    int         (*mod)(t_specifications *, va_list *);
+}               t_func_list;
+*/
+
+/*ft_printf.c*/
+int 	        ft_istype(char c);
+int             ft_isflag(char c);
+int		        formated(char **string, t_specifications *fmt);
 int		        ft_printf(char *format, ...);
+
+/*find_function.c*/
+int		c(t_specifications *data, va_list *ap);
+int		find_function(t_specifications *data, va_list *ap);
 
 #endif
