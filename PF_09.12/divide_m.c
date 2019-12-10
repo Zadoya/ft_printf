2563f471a955334204619252cd2276d9b9a002f8
Dunn Orphan <dorphan@mi-a2.21-school.ru>
2019-12-10 20:28:27 +0300

    dividing and multiplication

--- PF_09.12/divide_m.c
+++ PF_09.12/divide_m.c
@@ -12,43 +12,91 @@
 
 #include "ft_printf.h"
 
+static char		*minus_dec(char *m, int len_m)
+{
+	if (m[len_m - 1] == '0')
+		minus_dec(m, len_m - 1);
+	m[len_m - 1] -= 1;
+	m[len_m] += 10;
+	return (m);
+}
+
 static char 	*substraction(char *m, char *second_arg)
 {
 	int 		len_m;
 	int 		len_second;
 
-	len_m = ft_strlen(m);
-	len_second = ft_strlen(second_arg);
+	len_m = ft_strlen(m) - 1;
+	len_second = ft_strlen(second_arg) - 1;
+	while (len_second >= 0)
+	{
+		if(m[len_m] < second_arg[len_second])
+			m = minus_dec(m, len_m);
+		m[len_m] = m[len_m] - second_arg[len_second--] + '0';
+		len_m--;
+	}
+	while (*m == '0')
+		m++;
+	return (m);
+}
 
+static char		circle_substraction(char *m, char *second_arg)
+{
+	char		result;
 
+	result = 0;
+	if (*m == '0')
+		m++;
+	while ((ft_strlen(m) > 19) || (ft_strcmp(m, second_arg) >= 0))
+	{
+		m = substraction(m, second_arg);
+		result++;
+	}
+	return (result + '0');
 }
 
 
-char 			*divide_m(char *m, t_specifications *data)
+static void 	move_mantissa(char *m, char *second_arg)
 {
-	char 		result[data->precision + 22];
+	char 		*tmp;
+	char 		*tmp2;
+	char 		*tmp3;
+
+	tmp = m;
+	tmp2 = m + 1;
+	while (*tmp == '0')
+		tmp++;
+	tmp3 = tmp;
+	while (*(tmp3 + 1) != '\0')
+		tmp3++;
+	tmp2 = ft_memcpy(tmp2, tmp, tmp3 - tmp);
+	if (tmp3 - tmp > tmp - tmp2)
+		tmp = tmp2 + (tmp3 - tmp);
+	while (*tmp)
+		*tmp++ = '0';
+	if (ft_strcmp(tmp2, second_arg) < 0)
+		m = ft_memcpy(m, tmp2,tmp3 - tmp2);
+}
+
+char 			*divide_m(unsigned long long mantissa, t_specifications *data)
+{
+	char 		*result;
 	int 		i;
+	char 		*m;
+	char 		*tmp;
 
 	i = 0;
+	if (!(result = malloc(data->precision + 22)))
+		return (NULL);
+	result[data->precision + 22] = '\0';
 	result[i++] = '1';
 	result[i++] = '.';
-	m = substraction(m, "9223372036854775808");
-
-
-
-
-
-	/*
-	 * Этот код скорее всего бесполезен.
-	 *
-	 * if (ft_strlen(m) > 19)
-		result[i++] = '1';
-	else
+	m = ft_ulltoa_base(mantissa - 9223372036854775808, 10);
+	m = ft_concat_and_freehead(m, "0");
+	while (i < data->precision + 22)
 	{
-		if (ft_strcmp(m, "9223372036854775808") < 0)
-			result[i++] = '0';
-		else
-			result[i++] = '1';
+		result[i++] = circle_substraction(m, "9223372036854775808");
+		move_mantissa(m, "9223372036854775808");
 	}
-	result[i++] = '.';*/
+	return (result);
 }
---
+++ PF_09.12/multiplication_m_by_e.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   multiolication_m_by_e.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dorphan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/10 17:34:02 by dorphan           #+#    #+#             */
+/*   Updated: 2019/12/10 17:34:04 by dorphan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static char	*addition(char *result, char *mantissa)
+{
+	int		tmp_r;
+	int		tmp_m;
+
+	tmp_r = ft_strlen(result) - 1;
+	tmp_m = ft_strlen(mantissa) - 1;
+	ft_putstr(mantissa);
+	while (tmp_m >= 0)
+	{
+		if (mantissa[tmp_m] != '.')
+		{
+			result[tmp_r] = result[tmp_r] + mantissa[tmp_m] - '0';
+			if (result[tmp_r] > '9')
+			{
+				result[tmp_r] = result[tmp_r] - 10;
+				if (result[tmp_r - 1] == '.')
+					result[tmp_r - 2] += 1;
+				else if (!tmp_r)
+				{
+					result = ft_concat_and_free("0", result);
+					result[tmp_r] += 1;
+					tmp_r++;
+				}
+				else
+					result[tmp_r - 1] += 1;
+			}
+		}
+		tmp_m--;
+		tmp_r--;
+	}
+	return (result);
+}
+
+
+char		*multiplication(char *mantissa, int exponent)
+{
+	char	*result;
+
+	if (exponent == 0)
+		return ("0");
+	result = ft_strdup(mantissa);
+	while (exponent > 1)
+	{
+		result = addition(result, mantissa);
+		exponent--;
+	}
+	free(mantissa);
+	return (result);
+}
